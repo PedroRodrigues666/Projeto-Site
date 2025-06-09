f1041e3b16febd69fd5b5504a2dc501781f92380
Pedro Henrique Rodrigues Carvalho <pedro9831398@gmail.com>
2025-06-09 01:07:44 -0300

    alterações finais

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const botao = document.getElementById('toggle-folhas');
+    const folhas = document.querySelectorAll('.folha-box');
+    let visivel = false;
+
+    botao.addEventListener('click', function () {
+        visivel = !visivel;
+
+        folhas.forEach(folha => {
+            folha.style.display = visivel ? 'block' : 'none';
+        });
+
+        botao.textContent = visivel ? 'Ver Menos' : 'Veja Mais';
+    });
+
+    folhas.forEach(folha => {
+        folha.style.display = 'none';
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
